b2bbd88cb735e6a36d7921e361355bb416f5a614
Lyncon Santos <io.lynconr@gmail.com>
2019-02-09 20:49:00 -0200

    consuming api

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
 const express = require('express');
 const engines = require('consolidate');
+const requestPromise = require('request-promise');
 
 const firebaseApp = firebase.initializeApp(functions.config().firebase);
 
@@ -10,17 +11,30 @@ app.engine('hbs',engines.handlebars);
 app.set('views','./views');
 app.set('view engine','hbs');
 
-app.get('/timestamp',(request, response) => {
-    response.send(`${Date.now()}`);
+app.get('/',(request, response) => {
+    response.set('Cache-control','public, max-age=300, s-maxage=600');
+    response.render('index');
 });
 
-app.get('/',(request, response) => {
-    // response.set('Cache-control','public, max-age=300, s-maxage=600');
+var options = {
+     method :   'GET'
+    ,uri    :   'https://torre.bio/api/people'
+    ,json   :   true
+};
 
-    const helloTorre = "Hello Torrexesss";
-    const statusOk = "My environment is up and running - Happy to take this exam =D";
-
-    response.render('index', { helloTorre, statusOk });
+app.get('/listAll', (request,response)=>{
+    requestPromise(options)
+        .then(function(parseBody){
+            var peopleData = parseBody;
+            
+            peopleData.forEach(element => {
+                const name = element.name;
+            });
+            response.render('listAll');
+        })
+        .catch((error) => {
+            return console.log(error);
+        })
 });
 
 exports.app = functions.https.onRequest(app);
