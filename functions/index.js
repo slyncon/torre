8c38a2211cbdad9f5dbdc4867a78571bbf666fa6
Lyncon Santos <io.lynconr@gmail.com>
2019-02-09 23:06:47 -0200

    .

--- functions/index.js
+++ functions/index.js
@@ -32,7 +32,7 @@ app.post('/searchTorrex', (request,response) =>{
     };
 
     requestPromise(options)
-        .then(function(peopleData) {
+        .then((peopleData) =>{
             response.render('searchTorrex',{peopleData});
         })
         .catch((error) => {
